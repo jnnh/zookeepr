f8cf82ec693dbc512f9dda545446fe17b887c144
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-02-13 14:17:40 -0500

    integrated front-end

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+//instruct server to make these files static resources
+app.use(express.static('public'));
 
 const { animals } =require('./data/animals');
 
@@ -101,6 +103,22 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
     }
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
